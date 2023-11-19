fb98e379d3be53cc7945ec4419e00258a04ecfc9
jgomezz <jgomezz@gmail.com>
2023-11-19 11:46:21 -0500

    Update

--- src/main/java/pe/edu/tecsup/tienda/util/ConexionBD.java
+++ src/main/java/pe/edu/tecsup/tienda/util/ConexionBD.java
@@ -4,22 +4,48 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
 public class ConexionBD {
-	
+
 	public static Connection obtenerConexion() throws SQLException {
 		Connection con = null;
 		try {
 
-			Class.forName("com.mysql.jdbc.Driver");
+			Context initContext = new InitialContext();
+		
+			Context envContext = (Context) initContext.lookup("java:/comp/env");
 			
-			con = DriverManager.getConnection("jdbc:mysql://localhost/tienda?useSSL=false", "root", "");
-
-		} catch (ClassNotFoundException e) {
+			DataSource datasource = (DataSource) envContext.lookup("jdbc/tienda");
+			
+			con = datasource.getConnection();
+		
+		} catch (NamingException e) {
 			e.printStackTrace();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
 		return con;
 	}
+
+	/*
+	 * public static Connection obtenerConexion() throws SQLException { Connection
+	 * con = null; try {
+	 * 
+	 * Class.forName("com.mysql.jdbc.Driver");
+	 * 
+	 * con =
+	 * DriverManager.getConnection("jdbc:mysql://localhost/tienda?useSSL=false",
+	 * "root", "");
+	 * 
+	 * } catch (ClassNotFoundException e) { e.printStackTrace(); } catch (Exception
+	 * e) { e.printStackTrace(); }
+	 * 
+	 * return con; }
+	 * 
+	 */
+
 }

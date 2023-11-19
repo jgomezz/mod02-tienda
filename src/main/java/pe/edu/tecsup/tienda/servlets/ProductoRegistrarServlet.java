07ef07a9249d102785adbfcd9038a3dc5baf4ceb
jgomezz <jgomezz@gmail.com>
2023-11-19 08:34:55 -0500

    Update

---
+++ src/main/java/pe/edu/tecsup/tienda/servlets/ProductoRegistrarServlet.java
@@ -0,0 +1,64 @@
+package pe.edu.tecsup.tienda.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+
+import pe.edu.tecsup.tienda.services.CategoriaService;
+import pe.edu.tecsup.tienda.services.ProductoService;
+
+/**
+ * Servlet implementation class ProductoRegistrarServlet
+ */
+@WebServlet("/ProductoRegistrarServlet")
+public class ProductoRegistrarServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	private static final Logger log = Logger.getLogger(ProductoListarServlet.class);
+
+	private CategoriaService categoriaService;
+	private ProductoService productoService;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public ProductoRegistrarServlet() {
+		this.productoService = new ProductoService();
+		this.categoriaService = new CategoriaService();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		log.info("Get ProductoRegistrarServlet");
+	
+	
+		request.getRequestDispatcher("/WEB-INF/jsp/producto/registrar.jsp").
+				forward(request, response);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//doGet(request, response);
+	
+	
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/jsp/producto/registrar.jsp
@@ -0,0 +1,19 @@
+<%@page import="pe.edu.tecsup.tienda.entities.Producto"%>
+<%@page import="java.util.List"%>
+
+<!DOCTYPE html>
+<html>
+<head>
+
+	<%@include file="/WEB-INF/jsp/includes/head.jsp"%>
+	<meta charset="UTF-8">
+	<title>Registrar Servlet</title>
+</head>
+<body>
+
+	<%@include file="/WEB-INF/jsp/includes/navbar.jsp"%>
+
+
+
+</body>
+</html>
